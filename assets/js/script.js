e490e5225e5a0d0be79f5ad50ec205efac7bd9c2
Chris Martinez <cbmartinez42@gmail.com>
2021-03-11 10:36:02 -0600

    updating with framework and some pseudocoding

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// favorites in the sidebar
+
+
+// main splash page functionality/api. Do we want to replace the splash page, or just have it pop up with modals?
+
+
+// script to save favorites in localStorage when selected
+
+
