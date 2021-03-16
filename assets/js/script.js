26eaf173adfce1c972a3eafa2159a43f14c8e0b6
Chris Martinez <cbmartinez42@gmail.com>
2021-03-16 09:24:38 -0500

    minor update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+<div class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Modal title</p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+      <!-- Content ... -->
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>
+
+
+
+
 // declare variables
 const rdmImageCol = document.getElementById('rdm-image')
 
