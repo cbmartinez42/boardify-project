23b462fdd4c5841027fcc836f41b6436bf4dade8
Stacy Martin <sbrown1031@gmail.com>
2021-03-17 00:04:36 -0500

    updated api js and added random images

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,6 @@ let bored = function (event) {
       })
       .then(function (data) {
               console.log(data);
-      
         //this empties the contents of the row
         $(splash).empty();
         boredDisplay(data);
@@ -66,8 +65,9 @@ let boredDisplay = function (data) {
         <p>Number of Participants; ${participants}</p>
         <p>Type of Activity; ${type}</p>`)
 }
-
 // end bored API functions
+
+
 
 // roboHash API functions 
 const roboHash = function (event){
@@ -103,16 +103,106 @@ const roboHash = function (event){
   div.classList.add('roboCont');
     div.appendChild(col);
     splash.append(div);
-  
-
- 
-  
-
-
 }
+// end roboHash API functions
 
+
+
+// // superHero API functions
+let superHero = function (event) {
+  let apiURL = `http://superheroapi.com/api/10224580750642127`;
+
+  fetch(apiURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+            console.log(data);
+          $(splash).empty();
+      superHeroDisplay(data);
+    })
+};
+
+let superHeroDisplay = function (data) { 
+  const id = data.id;
+  const biography = data.biography;
+  const characterImage = data.characterImage;
+  const work = data.work;
+  const container = $("<div>")
+      container.html(`<h3>Superhero Superfun, Check Here</h3>
+      <p>${id}</p>
+      <p>Characters Biography; ${biography}</p>
+      <img>Superhero Image; ${characterImage}</img>
+      <p>Character's Work Occupation and Operation Base;  ${work}</p>`)
+}
+// end superHero API function
+
+
+
+// cat facts API function
+let catFacts = function (event) {
+  let apiURL = `http://cat-fact.herokuapp.com`;
+
+  fetch(apiURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+            console.log(data);
+          $(splash).empty();
+      catFactsDisplay(data);
+    })
+};
+
+let catFactsDisplay = function (data) { 
+  const facts = data.facts;
+  const container = $("<div>")
+      container.html(`<h3>Click Fur a Cat Fact!</h3>`)
+}
+// Is this all the API provides??
+// end cat facts API function
+
+
+
+// random jokes API function
+let randomJokes = function (event) {
+  let apiURL = `https://jokeapi-v2.p.rapidapi.com/submit`;
+
+  fetch(apiURL)
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+            console.log(data);
+          $(splash).empty();
+      randomJokesDisplay(data);
+    })
+};
+
+let randomJokesDisplay = function (data) { 
+  const category = data.category;
+  const type = data.type;
+  const joke = data.joke;
+  const container = $("<div>")
+      container.html(`<h3>Ready for a Laugh?!</h3>
+      <p>Joke Category; ${category}</p>
+      <p>Type of Joke; ${type}</p>
+      <p>Here goes funny... ; ${joke}</p>`)
+}
+// there are a lot of joke APIs, is this the one we want
+// there are flag options to look into for blocking NSFW jokes
+// end random jokes API function
 
 
 // eventlisteners go here
 document.getElementById('bored').addEventListener('click', bored)
 document.getElementById('roboHash').addEventListener('click', roboHash)
+document.getElementById('superHero').addEventListener('click', superHero)
+document.getElementById('catFacts').addEventListener('click', catFacts)
+document.getElementById('randomJokes').addEventListener('click', randomJokes)
